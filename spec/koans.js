6dffcdfee56934576b5b8191c89c2ef9a4935dd4
Pablo García <heypablete@gmail.com>
2018-04-09 19:18:55 +0200

    estoy en: 'swap two variables, in one operation'

--- spec/koans.js
+++ spec/koans.js
@@ -257,12 +257,12 @@ describe('The object literal allows for new shorthands. ', () => {
 
   describe('with variables', () => {
     it('the short version for `{y: y}` is {y}', () => {
-      /*.....*/
-      //expect(short).toEqual({y: y});
+      const short = {y: y}; // o también {y}
+      expect(short).toEqual({y: y});
     });
     it('works with multiple variables too', () => {
-      /*.....*/
-      //expect(short).toEqual({x: x, y: y});
+      const short = {x, y};
+      expect(short).toEqual({x: x, y: y});
     });
   });
   
@@ -271,13 +271,13 @@ describe('The object literal allows for new shorthands. ', () => {
     const func = () => func;
 
     it('using the name only uses it as key', () => {
-      /*.......*/
-      //expect(short).toEqual({func: func});
+      const short = {func}
+      expect(short).toEqual({func: func});
     });
     
     it('a different key must be given explicitly, just like before ES6', () => {
-      /*.......*/
-      //expect(short).toEqual({otherKey: func});
+      const short = {otherKey: func}
+      expect(short).toEqual({otherKey: func});
     });
   });
   
@@ -286,8 +286,8 @@ describe('The object literal allows for new shorthands. ', () => {
 describe('destructuring arrays makes shorter code. ', () => {
 
   it('extract value from array, e.g. extract 0 into x like so `let [x] = [0];`', () => {
-    let firstValue = [1];
-    //expect(firstValue).toEqual(1);
+    let [firstValue] = [1];
+    expect(firstValue).toEqual(1);
   });
 
   it('swap two variables, in one operation', () => {
@@ -311,8 +311,8 @@ describe('destructuring arrays makes shorter code. ', () => {
   });
 
   it('chained assignments', () => {
-    let c, d;
-    let a, b = c, d = [1, 2];
+    //let c, d;
+    //let a, b = c, d = [1, 2];
     //expect([a, b, c, d]).toEqual([1, 2, 1, 2]);
   });
 
