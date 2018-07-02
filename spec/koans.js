fe36d4da4e56d50124045692117ba814fbd1b76b
Verónica Reina <veroreinah@gmail.com>
2018-07-02 16:17:44 +0200

    ES6 arrow functions

--- spec/koans.js
+++ spec/koans.js
@@ -338,41 +338,41 @@ describe('arrow functions. ', () => {
 
   it('are shorter to write', function() {
     let func = () => {
-      /*........*/
+      return 'I am func';
     };
-    //expect(func()).toBe('I am func');
+    expect(func()).toBe('I am func');
   });
 
   it('a single expression, without curly braces returns too', function() {
-    /*let func = () => .........;*/
-    //expect(func()).toBe('I return too');
+    let func = () => 'I return too';
+    expect(func()).toBe('I return too');
   });
 
   it('one parameter can be written without parens', () => {
-   /* let func = ........;*/
-    //expect(func(25)).toEqual(24)
+    let func = number => number - 1;
+    expect(func(25)).toEqual(24)
   });
 
   it('many params require parens', () => {
-    /* let func = ........;*/
-    //expect(func(23,42)).toEqual(23+42)
+    let func = (a, b) => a + b;
+    expect(func(23,42)).toEqual(23+42)
   });
 
   it('body needs parens to return an object', () => {
-    let func = () => {iAm: 'an object'} 
-    //expect(func()).toEqual({iAm: 'an object'});
+    let func = () => {
+      return {iAm: 'an object'};
+    }; 
+    expect(func()).toEqual({iAm: 'an object'});
   });
 
   class LexicallyBound {
   
     getFunction() {
-      return () => {
-        return new LexicallyBound() /*changes might go here*/
-      }
+      return () => this
     }
   
     getArgumentsFunction() {
-      return function() { return arguments } /*or here*/
+      return () => { return arguments }
     } 
   }
 
@@ -382,23 +382,23 @@ describe('arrow functions. ', () => {
       let bound = new LexicallyBound();
       let fn = bound.getFunction();
       
-      //expect(fn()).toBe(bound);
+      expect(fn()).toBe(bound);
     });
   
     it('can NOT bind a different context', function() {
       let bound = new LexicallyBound();
       let fn = bound.getFunction();
       let anotherObj = {};
-      let expected = anotherObj; //change this
+      let expected = bound; //change this
       
-      //expect(fn.call(anotherObj)).toBe(expected);
+      expect(fn.call(anotherObj)).toBe(expected);
     });
     
     it('`arguments` doesnt work inside arrow functions', function() {
       let bound = new LexicallyBound();
       let fn = bound.getArgumentsFunction();
       
-      //expect(fn(1, 2).length).toEqual(0);
+      expect(fn(1, 2).length).toEqual(0);
     });
     
   });
