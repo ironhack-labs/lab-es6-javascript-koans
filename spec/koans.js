96b9c035290c8ea918322f8b4e744a6a09d001bc
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-11-13 09:33:12 +0100

    faltan algunos ejercicios

--- spec/koans.js
+++ spec/koans.js
@@ -308,38 +308,37 @@ describe("destructuring can also have default values. ", () => {
 describe("arrow functions. ", () => {
   it("are shorter to write", function() {
     let func = () => {
-      /*........*/
+      return 'I am func'
     };
-    // expect(func()).toBe('I am func');
+    expect(func()).toBe('I am func');
   });
 
   it("a single expression, without curly braces returns too", function() {
-    /*let func = () => .........;*/
-    //expect(func()).toBe('I return too');
+    let func = () => 'I return too';
+    expect(func()).toBe('I return too');
   });
 
   it("one parameter can be written without parens", () => {
-    /* let func = ........;*/
-    //expect(func(25)).toEqual(24)
+    let func = (a) => (--a);
+    expect(func(25)).toEqual(24)
   });
 
   it("many params require parens", () => {
-    /* let func = ........;*/
-    //expect(func(23,42)).toEqual(23+42)
+    let func = (a ,b) => a + b;
+    expect(func(23,42)).toEqual(23+42)
   });
 
   it("body needs parens to return an object", () => {
     let func = () => {
-      iAm: "an object";
+      return {iAm: "an object"};
     };
-    // expect(func()).toEqual({iAm: 'an object'});
+    expect(func()).toEqual({iAm: 'an object'});
   });
 
   class LexicallyBound {
     getFunction() {
-      return () => {
-        return new LexicallyBound(); /*changes might go here*/
-      };
+      return () => this; /*changes might go here*/
+    
     }
 
     getArgumentsFunction() {
@@ -354,7 +353,7 @@ describe("arrow functions. ", () => {
       let bound = new LexicallyBound();
       let fn = bound.getFunction();
 
-      //expect(fn()).toBe(bound);
+      expect(fn()).toBe(bound);
     });
 
     it("can NOT bind a different context", function() {
@@ -378,17 +377,17 @@ describe("arrow functions. ", () => {
 describe("destructuring function parameters. ", () => {
   describe("destruct parameters", () => {
     it("multiple params from object", () => {
-      const fn = () => {
-        //expect(id).toEqual(42);
-        //expect(name).toEqual('Wolfram');
+      const fn = ({id, name}) => {
+        expect(id).toEqual(42);
+        expect(name).toEqual('Wolfram');
       };
       const user = { name: "Wolfram", id: 42 };
       fn(user);
     });
 
     it("multiple params from array/object", () => {
-      const fn = ([]) => {
-        //expect(name).toEqual('Alice');
+      const fn = ([,{name}]) => {
+        expect(name).toEqual('Alice');
       };
       const users = [{ name: "nobody" }, { name: "Alice", id: 42 }];
       fn(users);
@@ -397,9 +396,9 @@ describe("destructuring function parameters. ", () => {
 
   describe("default values", () => {
     it("for simple values", () => {
-      const fn = (id, name) => {
-        //expect(id).toEqual(23);
-        //expect(name).toEqual('Bob');
+      const fn = (id, name = "Bob") => {
+        expect(id).toEqual(23);
+        expect(name).toEqual('Bob');
       };
       fn(23);
     });
@@ -413,10 +412,10 @@ describe("destructuring function parameters. ", () => {
     });
 
     it("mix of parameter types", () => {
-      const fn = (id, [arr], { obj }) => {
-        //expect(id).toEqual(1);
-        //expect(arr).toEqual(2);
-        //expect(obj).toEqual(3);
+      const fn = (id = 1, [arr = 2], { obj = 3 }) => {
+        expect(id).toEqual(1);
+        expect(arr).toEqual(2);
+        expect(obj).toEqual(3);
       };
       fn(void 0, [], {});
     });
@@ -426,13 +425,13 @@ describe("destructuring function parameters. ", () => {
 describe("assign object property values to new variables while destructuring. ", () => {
   describe("for simple objects", function() {
     it("use a colon after the property name, like so `propertyName: newName`", () => {
-      const { x } = { x: 1 };
-      //expect(y).toEqual(1);
+      const { x, y } = {x: 1, y: 1};
+      expect(y).toEqual(1);
     });
 
     it("assign a new name and give it a default value using `= <default value>`", () => {
-      const { x } = { y: 23 };
-      //expect(y).toEqual(42);
+      const { x = 20, y } = { y: 42 };
+      expect(y).toEqual(42);
     });
   });
 
