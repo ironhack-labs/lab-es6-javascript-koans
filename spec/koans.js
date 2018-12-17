ce23c9c4aacb77a119a81a84aeb57eb6510c96bd
Nerea Aparicio <nerea.aparicio@acuntia.dom>
2018-12-17 17:23:24 +0100

    10th describe done

--- spec/koans.js
+++ spec/koans.js
@@ -300,35 +300,35 @@ context = describe;
 
 // });
 
-describe('destructuring can also have default values. ', () => {
+// describe('destructuring can also have default values. ', () => {
 
-  it('for an empty array', () => {
-    const [a] = [];
-    //expect(a).toEqual(1)
-  });
+//   it('for an empty array', () => {
+//     const [a] = [1];
+//     expect(a).toEqual(1)
+//   });
 
-  it('for a missing value', () => {
-    const [a,b,c] = [1,,3];
-    //expect(b).toEqual(2);
-  });
+//   it('for a missing value', () => {
+//     const [a,b=2,c] = [1,,3];
+//     expect(b).toEqual(2);
+//   });
 
-  it('in an object', () => {
-    const [a, b] = [{a: 1}];
-    //expect(b).toEqual(2);
-  });
+//   it('in an object', () => {
+//     const [a, b=2] = [{a: 1},];
+//     expect(b).toEqual(2);
+//   });
 
-  it('if the value is undefined', () => {
-    const {a, b} = {a: 1, b: void 0};
-    //expect(b).toEqual(2);
-  });
+//   it('if the value is undefined', () => {
+//     const {a, b=2} = {a: 1, b: void 0};
+//     expect(b).toEqual(2);
+//   });
 
-  it('also a string works with defaults', () => {
-    const [a, b] = '1';
-    //expect(a).toEqual('1');
-    // expect(b).toEqual(2);
-  });
+//   it('also a string works with defaults', () => {
+//     const [a, b=2] = '1';
+//     expect(a).toEqual('1');
+//     expect(b).toEqual(2);
+//   });
 
-});
+// });
 
 /*********************************
 ********* DAILY EXERCISE *********
