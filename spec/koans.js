6dede0bc70e0421f3a70394aa74761704993c806
Zenchy <alvaro.corral.soilan@gmail.com>
2019-01-29 09:39:55 +0100

    Dani Iglesias y Alvaro Zenchy, hasta la 296

--- spec/koans.js
+++ spec/koans.js
@@ -275,23 +275,27 @@ describe('destructuring also works on strings. ', () => {
 describe('destructuring objects. ', () => {
 
   it('is simple', () => {
-    const x = {x: 1};
+    const {z,x} = {x:1};
+    
+
     expect(x).toEqual(1);
   });
 
   describe('nested', () => {
     it('multiple objects', () => {
       const magic = {first: 23, second: 42};
-      /*const first, second  = ??????*/
-      //expect(second).toEqual(42);
+      const {first, second} = magic
+      expect(second).toEqual(42);
     });
     it('object and array', () => {
-      const {z:x} = {z: [23, 42]};
-      //expect(x).toEqual(42);
+      const {z:[,x]} = {z: [23, 42]};
+      
+      expect(x).toEqual(42);
     });
     it('array and object', () => {
-      const lang = [null, [{env: 'browser', lang: 'ES6'}]];
-      //expect(lang).toEqual('ES6');
+      const [,[{,lang}]] = [null, [{env: 'browser', lang: 'ES6'}]];
+      
+      expect(lang).toEqual('ES6');
     });
   });
 
