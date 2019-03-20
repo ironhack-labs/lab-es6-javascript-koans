ec1d62bf915fd3407bbbcfa45520d41ed1412277
Nouri T <zorisse@gmail.com>
2019-03-20 07:58:58 +0100

    296

--- spec/koans.js
+++ spec/koans.js
@@ -215,13 +215,13 @@ describe('The object literal allows for new shorthands. ', () => {
     const func = () => func;
 
     it('using the name only uses it as key', () => {
-      /*.......*/
-      //expect(short).toEqual({func: func});
+      const short = { func }
+      expect(short).toEqual({ func: func });
     });
 
     it('a different key must be given explicitly, just like before ES6', () => {
-      /*.......*/
-      //expect(short).toEqual({otherKey: func});
+      const short = { otherKey: func }
+      expect(short).toEqual({ otherKey: func });
     });
   });
 
@@ -231,33 +231,35 @@ describe('destructuring arrays makes shorter code. ', () => {
 
   it('extract value from array, e.g. extract 0 into x like so `let [x] = [0];`', () => {
     let firstValue = [1];
-    //expect(firstValue).toEqual(1);
+    [firstValue] = firstValue
+    expect(firstValue).toEqual(1);
   });
 
   it('swap two variables, in one operation', () => {
     let [x, y] = ['ax', 'why'];
-    [x, y] = [x, y];
-    //expect([x, y]).toEqual(['why', 'ax']);
+    [x, y] = [y, x];
+    expect([x, y]).toEqual(['why', 'ax']);
   });
 
   it('leading commas', () => {
     const all = ['ax', 'why', 'zet'];
-    const [z] = all;
-    //expect(z).toEqual('zet');
+    const [, , z] = all;
+    expect(z).toEqual('zet');
   });
 
   it('extract from nested arrays', () => {
-    const user = [['Some', 'One'], 23];
+    const user = ['Some', 'One', 23];
     const [firstName, surname, age] = user;
 
     const expected = 'Some One = 23 years';
-    //expect(`${firstName} ${surname} = ${age} years`).toEqual(expected);
+    expect(`${firstName} ${surname} = ${age} years`).toEqual(expected);
   });
 
   it('chained assignments', () => {
     let c, d;
-    // let a, b = c, d = [1, 2];
-    //expect([a, b, c, d]).toEqual([1, 2, 1, 2]);
+    let [a, b] = [c, d] = [1, 2];
+
+    expect([a, b, c, d]).toEqual([1, 2, 1, 2]);
   });
 
 });
@@ -265,13 +267,13 @@ describe('destructuring arrays makes shorter code. ', () => {
 describe('destructuring also works on strings. ', () => {
 
   it('destructure every character', () => {
-    let a, b, c = 'abc';
-    //expect([a, b, c]).toEqual(['a', 'b', 'c']);
+    let [a, b, c] = 'abc';
+    expect([a, b, c]).toEqual(['a', 'b', 'c']);
   });
 
   it('missing characters are undefined', () => {
-    const [a, c] = 'ab';
-    //expect(c).toEqual(void 0);
+    const [a, b, c] = 'ab';
+    expect(c).toEqual(void 0);
   });
 });
 
@@ -279,14 +281,15 @@ describe('destructuring objects. ', () => {
 
   it('is simple', () => {
     const x = { x: 1 };
-    //expect(x).toEqual(1);
+
+    expect(x.x).toEqual(1);
   });
 
   describe('nested', () => {
     it('multiple objects', () => {
       const magic = { first: 23, second: 42 };
-      /*const first, second  = ??????*/
-      //expect(second).toEqual(42);
+      const { first, second } = magic
+      expect(second).toEqual(42);
     });
     it('object and array', () => {
       const { z: x } = { z: [23, 42] };
