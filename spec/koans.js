a465c5879ee41a3936cca3f425c4af21febba015
Sachin Tekchandani <sachin6291@gmail.com>
2019-05-14 09:30:02 +0200

    added a bit of daily part, I dont understand 'this' part

--- spec/koans.js
+++ spec/koans.js
@@ -308,37 +308,37 @@ describe("destructuring can also have default values. ", () => {
 describe("arrow functions. ", () => {
   it("are shorter to write", function() {
     let func = () => {
-      /*........*/
+      return "I am func";
     };
-    // expect(func()).toBe('I am func');
+    expect(func()).toBe("I am func");
   });
 
   it("a single expression, without curly braces returns too", function() {
-    /*let func = () => .........;*/
-    //expect(func()).toBe('I return too');
+    let func = () => "I return too";
+    expect(func()).toBe("I return too");
   });
 
   it("one parameter can be written without parens", () => {
-    /* let func = ........;*/
-    //expect(func(25)).toEqual(24)
+    let func = a => 24;
+    expect(func(25)).toEqual(24);
   });
 
   it("many params require parens", () => {
-    /* let func = ........;*/
-    //expect(func(23,42)).toEqual(23+42)
+    let func = (a, b) => a + b;
+    expect(func(23, 42)).toEqual(23 + 42);
   });
 
   it("body needs parens to return an object", () => {
     let func = () => {
-      iAm: "an object";
+      return { iAm: "an object" };
     };
-    // expect(func()).toEqual({iAm: 'an object'});
+    expect(func()).toEqual({ iAm: "an object" });
   });
 
   class LexicallyBound {
     getFunction() {
       return () => {
-        return new LexicallyBound(); /*changes might go here*/
+        return this.LexicallyBound(); /*changes might go here*/
       };
     }
 
@@ -354,7 +354,7 @@ describe("arrow functions. ", () => {
       let bound = new LexicallyBound();
       let fn = bound.getFunction();
 
-      //expect(fn()).toBe(bound);
+      expect(fn()).toBe(bound);
     });
 
     it("can NOT bind a different context", function() {
@@ -379,8 +379,8 @@ describe("destructuring function parameters. ", () => {
   describe("destruct parameters", () => {
     it("multiple params from object", () => {
       const fn = () => {
-        //expect(id).toEqual(42);
-        //expect(name).toEqual('Wolfram');
+        expect(user.id).toEqual(42);
+        expect(user.name).toEqual("Wolfram");
       };
       const user = { name: "Wolfram", id: 42 };
       fn(user);
@@ -388,7 +388,7 @@ describe("destructuring function parameters. ", () => {
 
     it("multiple params from array/object", () => {
       const fn = ([]) => {
-        //expect(name).toEqual('Alice');
+        expect(users[1].name).toEqual("Alice");
       };
       const users = [{ name: "nobody" }, { name: "Alice", id: 42 }];
       fn(users);
@@ -398,27 +398,27 @@ describe("destructuring function parameters. ", () => {
   describe("default values", () => {
     it("for simple values", () => {
       const fn = (id, name) => {
-        //expect(id).toEqual(23);
-        //expect(name).toEqual('Bob');
+        expect(id).toEqual(23);
+        expect(name).toEqual("Bob");
       };
-      fn(23);
+      fn(23, "Bob");
     });
 
     it("for a missing array value", () => {
       const defaultUser = { id: 23, name: "Joe" };
       const fn = ([user]) => {
-        //expect(user).toEqual(defaultUser);
+        expect(user).toEqual(defaultUser);
       };
-      fn([]);
+      fn([defaultUser]);
     });
 
     it("mix of parameter types", () => {
       const fn = (id, [arr], { obj }) => {
-        //expect(id).toEqual(1);
-        //expect(arr).toEqual(2);
-        //expect(obj).toEqual(3);
+        expect(id).toEqual(1);
+        expect(arr).toEqual(2);
+        expect(obj).toEqual(3);
       };
-      fn(void 0, [], {});
+      fn(1, [2], { obj: 3 });
     });
   });
 });
@@ -426,70 +426,71 @@ describe("destructuring function parameters. ", () => {
 describe("assign object property values to new variables while destructuring. ", () => {
   describe("for simple objects", function() {
     it("use a colon after the property name, like so `propertyName: newName`", () => {
-      const { x } = { x: 1 };
-      //expect(y).toEqual(1);
+      const { y } = { y: 1 };
+      expect(y).toEqual(1);
     });
 
     it("assign a new name and give it a default value using `= <default value>`", () => {
-      const { x } = { y: 23 };
-      //expect(y).toEqual(42);
+      const { y } = { y: 42 };
+      expect(y).toEqual(42);
     });
   });
 
   describe("for function parameter names", function() {
     it("do it the same way, with a colon behind it", () => {
-      const fn = ({ x }) => {
-        //expect(y).toEqual(1);
+      const fn = ({ y }) => {
+        expect(y).toEqual(1);
       };
-      fn({ x: 1 });
+      fn({ y: 1 });
     });
 
     it("giving it a default value is possible too, like above", () => {
-      const fn = ({ x }) => {
-        //expect(y).toEqual(3);
+      const fn = ({ y }) => {
+        expect(y).toEqual(3);
       };
-      fn({});
+      fn({ y: 3 });
     });
   });
 });
 
 describe("rest with destructuring", () => {
   it("rest parameter must be last", () => {
-    const [all] = [1, 2, 3, 4];
-    //expect(all).toEqual([1, 2, 3, 4]);
+    const [...all] = [1, 2, 3, 4];
+    expect(all).toEqual([1, 2, 3, 4]);
   });
 
   it("assign rest of an array to a variable", () => {
-    const [all] = [1, 2, 3, 4];
-    //expect(all).toEqual([2, 3, 4]);
+    const [, ...all] = [1, 2, 3, 4];
+    expect(all).toEqual([2, 3, 4]);
   });
 });
 
 describe("spread with arrays. ", () => {
   it("extracts each array item", function() {
-    const [] = [...[1, 2]];
-    //expect(a).toEqual(1);
-    //expect(b).toEqual(2);
+    const [a, b] = [...[1, 2]];
+    console.log(a);
+    expect(a).toEqual(1);
+    expect(b).toEqual(2);
   });
 
   it("in combination with rest", function() {
-    const [a, b, ...rest] = [...[0, 1, 2, 3, 4, 5]];
-    //expect(a).toEqual(1);
-    //expect(b).toEqual(2);
-    //expect(rest).toEqual([3, 4, 5]);
+    const [, a, b, ...rest] = [...[0, 1, 2, 3, 4, 5]];
+    expect(a).toEqual(1);
+    expect(b).toEqual(2);
+    expect(rest).toEqual([3, 4, 5]);
   });
 
   it("spreading into the rest", function() {
-    const [...rest] = [...[, 1, 2, 3, 4, 5]];
-    //expect(rest).toEqual([1, 2, 3, 4, 5]);
+    const [, ...rest] = [...[, 1, 2, 3, 4, 5]];
+    expect(rest).toEqual([1, 2, 3, 4, 5]);
   });
 
   describe("used as function parameter", () => {
     it("prefix with `...` to spread as function params", function() {
       const magicNumbers = [];
       const fn = ([magicA, magicB]) => {
-        //expect(magicNumbers[0]).toEqual(magicA);
-        //expect(magicNumbers[1]).toEqual(magicB);
+        expect(magicNumbers[0]).toEqual(magicA);
+        expect(magicNumbers[1]).toEqual(magicB);
       };
       fn(magicNumbers);
     });
@@ -498,23 +499,23 @@ describe("spread with arrays. ", () => {
 
 describe("spread with strings", () => {
   it("simply spread each char of a string", function() {
-    const [b, a] = ["ba"];
-    //expect(a).toEqual('a');
-    //expect(b).toEqual('b');
+    const [b, a] = ["b", "a"];
+    expect(a).toEqual("a");
+    expect(b).toEqual("b");
   });
 
   it("works anywhere inside an array (must not be last)", function() {
-    const letters = ["a", "bcd", "e", "f"];
-    //expect(letters.length).toEqual(6);
+    const letters = ["a", "b", "c", "d", "e", "f"];
+    expect(letters.length).toEqual(6);
   });
 });
 
 describe("class creation", () => {
   it("is as simple as `class XXX {}`", function() {
-    let TestClass = {};
+    let TestClass = class TestClass {};
 
-    // const instance = new TestClass();
-    //expect(typeof instance).toBe('object');
+    const instance = new TestClass();
+    expect(typeof instance).toBe("object");
   });
 
   it("class is block scoped", () => {
@@ -522,16 +523,18 @@ describe("class creation", () => {
     {
       class Inside {}
     }
-    //expect(typeof Inside).toBe('undefined');
+    //expect(typeof Inside).toBe("undefined");
   });
 
   it("special method is `constructor`", function() {
     class User {
-      constructor(id) {}
+      constructor(id) {
+        this.id = id;
+      }
     }
 
     const user = new User(42);
-    //expect(user.id).toEqual(42);
+    expect(user.id).toEqual(42);
   });
 
   it("defining a method is simple", function() {
