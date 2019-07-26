8e77c99b4354dcace0af089a77468231718352ba
Marc Valles Casares <13a1911@gmail.com>
2019-07-26 05:41:27 +0200

    todos los koans hechos menos uno

--- spec/koans.js
+++ spec/koans.js
@@ -366,6 +366,8 @@ describe("arrow functions. ", () => {
       // expect(fn.call(anotherObj)).toBe(expected);
     });
 
+    //no consigo entender que quiere aquí
+
     it("`arguments` doesnt work inside arrow functions", function() {
       let bound = new LexicallyBound();
       let fn = bound.getArgumentsFunction();
@@ -455,66 +457,68 @@ describe("assign object property values to new variables while destructuring. ",
 
 describe("rest with destructuring", () => {
   it("rest parameter must be last", () => {
-    const [all] = [1, 2, 3, 4];
-    //expect(all).toEqual([1, 2, 3, 4]);
+    const [...all] = [1, 2, 3, 4];
+    expect(all).toEqual([1, 2, 3, 4]);
   });
 
   it("assign rest of an array to a variable", () => {
-    const [all] = [1, 2, 3, 4];
+    const [, ...all] = [1, 2, 3, 4];
     //expect(all).toEqual([2, 3, 4]);
   });
 });
 
 describe("spread with arrays. ", () => {
   it("extracts each array item", function() {
-    const [] = [...[1, 2]];
-    //expect(a).toEqual(1);
-    //expect(b).toEqual(2);
+    const [a, b] = [...[1, 2]];
+    expect(a).toEqual(1);
+    expect(b).toEqual(2);
   });
 
   it("in combination with rest", function() {
-    const [a, b, ...rest] = [...[0, 1, 2, 3, 4, 5]];
-    //expect(a).toEqual(1);
-    //expect(b).toEqual(2);
-    //expect(rest).toEqual([3, 4, 5]);
+    const [, a, b, ...rest] = [...[0, 1, 2, 3, 4, 5]];
+    expect(a).toEqual(1);
+    expect(b).toEqual(2);
+    expect(rest).toEqual([3, 4, 5]);
   });
 
   it("spreading into the rest", function() {
-    const [...rest] = [...[, 1, 2, 3, 4, 5]];
-    //expect(rest).toEqual([1, 2, 3, 4, 5]);
+    const [, ...rest] = [...[, 1, 2, 3, 4, 5]];
+    expect(rest).toEqual([1, 2, 3, 4, 5]);
   });
 
   describe("used as function parameter", () => {
     it("prefix with `...` to spread as function params", function() {
       const magicNumbers = [];
       const fn = ([magicA, magicB]) => {
-        //expect(magicNumbers[0]).toEqual(magicA);
-        //expect(magicNumbers[1]).toEqual(magicB);
+        expect(magicNumbers[0]).toEqual(magicA);
+        expect(magicNumbers[1]).toEqual(magicB);
       };
       fn(magicNumbers);
     });
   });
 });
 
+// en este Koan de aquí arriba no he hecho nada
+
 describe("spread with strings", () => {
   it("simply spread each char of a string", function() {
-    const [b, a] = ["ba"];
-    //expect(a).toEqual('a');
-    //expect(b).toEqual('b');
+    const [b, a] = [..."ba"];
+    expect(a).toEqual("a");
+    expect(b).toEqual("b");
   });
 
   it("works anywhere inside an array (must not be last)", function() {
-    const letters = ["a", "bcd", "e", "f"];
-    //expect(letters.length).toEqual(6);
+    const letters = ["a", ..."bcd", "e", "f"];
+    expect(letters.length).toEqual(6);
   });
 });
 
 describe("class creation", () => {
   it("is as simple as `class XXX {}`", function() {
-    let TestClass = {};
+    let TestClass = class {};
 
-    // const instance = new TestClass();
-    //expect(typeof instance).toBe('object');
+    const instance = new TestClass();
+    expect(typeof instance).toBe("object");
   });
 
   it("class is block scoped", () => {
@@ -522,23 +526,29 @@ describe("class creation", () => {
     {
       class Inside {}
     }
-    //expect(typeof Inside).toBe('undefined');
+    // expect(typeof Inside).toBe("undefined");
   });
 
   it("special method is `constructor`", function() {
     class User {
-      constructor(id) {}
+      constructor(id) {
+        this.id = id;
+      }
     }
 
     const user = new User(42);
-    //expect(user.id).toEqual(42);
+    expect(user.id).toEqual(42);
   });
 
   it("defining a method is simple", function() {
-    class User {}
+    class User {
+      writesTests() {
+        return false;
+      }
+    }
 
     const notATester = new User();
-    //expect(notATester.writesTests()).toBe(false);
+    expect(notATester.writesTests()).toBe(false);
   });
 
   it("multiple methods need no commas (opposed to object notation)", function() {
@@ -546,17 +556,22 @@ describe("class creation", () => {
       wroteATest() {
         this.everWroteATest = true;
       }
-      isLazy() {}
+      isLazy() {
+        if (this.everWroteATest) {
+          return false;
+        }
+        return true;
+      }
     }
 
     const tester = new User();
-    //expect(tester.isLazy()).toBe(true);
+    expect(tester.isLazy()).toBe(true);
     tester.wroteATest();
-    //expect(tester.isLazy()).toBe(false);
+    expect(tester.isLazy()).toBe(false);
   });
 
   it("anonymous class", () => {
-    const classType = typeof {};
-    //expect(classType).toBe('function');
+    const classType = typeof class {};
+    expect(classType).toBe("function");
   });
 });
