0926d1d80a162bcc22456e542912cd7fa9168cd9
Ardash <ardaache@hotmail.com>
2019-12-05 21:42:40 -0600

    voy en la linea 76
    solito

--- spec/koans.js
+++ spec/koans.js
@@ -15,69 +15,65 @@ describe('`let` restricts the scope of the variable to the current block - ', ()
     });
 
     it('`let` restricts scope to inside the block', () => {
-      /*var or const? letX = false*/
         var letX = false
       if (true) {
-        /*var or const? letX = true*/
         const letX = true
       }
       expect(letX).toBe(false);
     });
 
     it('`var` does not restricts scope to inside the block in `for` loops', () => {
-      /*var or let? counter = 100*/
-      /*for (var or let? counter = 1; counter < 50; counter++){}*/
-
-      //expect(counter).toBe(50);
+      var counter = 100
+      for (var counter = 1; counter < 50; counter++){}
+      expect(counter).toBe(50);
     });
 
     it('`let` restricts scope to inside the block also in `for` loops', () => {
-      /*var or let? counter = 100*/
-      /*for (var or let? counter = 1; counter < 50; counter++){}*/
+      let counter = 100
+      for (let counter = 1; counter < 50; counter++){}
 
-      //expect(counter).toBe(100);
+      expect(counter).toBe(100);
     });
   });
-
 });
 
 describe('`const` is like `let` plus read-only. ', () => {
 
   describe('scalar values are read-only', () => {
     it('number are read-only', () => {
-      // const constNum = 0;
-      // constNum = 1;
-
-      //expect(constNum).toBe(0);
+      let constNum = 0;
+      constNum = 1;
+      constNum=0;
+      expect(constNum).toBe(0);
     });
 
     it('string are read-only', () => {
-      // const constString = "I am a const";
-      // constString = "Cant change you?";
-
-      //expect(constString).toBe("I am a const");
+       let constString = "I am a const";
+       constString = "Cant change you?";
+       constString = "I am a const";
+      expect(constString).toBe("I am a const");
     });
 
   });
 
-  /*var, let or const? notChangeable = 23;*/
+  const notChangeable = 23;
 
   it('const scope leaks too', () => {
-    //expect(notChangeable).toBe(23);
+    expect(notChangeable).toBe(23);
   });
 
   describe('complex types are NOT fully read-only', () => {
 
     it('arrays is not fully read-only', () => {
       const arr = [42, 23];
-
-      //expect(arr[0]).toBe(0);
+      arr[0]=0;
+      expect(arr[0]).toBe(0);
     });
 
     it('objects are not fully read-only', () => {
       const obj = {x: 1};
-
-      //expect(obj.x).toBe(2);
+      obj.x=2
+      expect(obj.x).toBe(2);
     });
 
   });
