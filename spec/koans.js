c21ae7d8cc12ce8ec5a5a4e34d5a98744d0ac1f8
esrfree <esrfree@gmail.com>
2020-01-20 22:31:53 -0500

    var does not restricts scope to inside the block in for loops

--- spec/koans.js
+++ spec/koans.js
@@ -25,10 +25,10 @@ describe('`let` restricts the scope of the variable to the current block - ', ()
     });
 
     it('`var` does not restricts scope to inside the block in `for` loops', () => {
-      /*var or let? counter = 100*/
-      /*for (var or let? counter = 1; counter < 50; counter++){}*/
+      var counter = 100;
+      for (var counter = 1; counter < 50; counter++){}
 
-      //expect(counter).toBe(50);
+      expect(counter).toBe(50);
     });
 
     it('`let` restricts scope to inside the block also in `for` loops', () => {
