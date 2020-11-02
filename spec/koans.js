2a3d2332bc4e6a82db8d5a0b5a0ccdeb6ac99447
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-02 12:41:16 -0600

    search for an empty string, is always true in an empty string

--- spec/koans.js
+++ spec/koans.js
@@ -102,15 +102,15 @@ describe('`string.includes()` finds string within another string. ', () => {
 
   describe('find a string', function() {
     it('that matches exactly', function() {
-      /* const findSome = .... => 'xyz'.includes();*/
-      //expect(findSome('xyz')).toBe(true);
+      // const findSome = (....) => 'xyz'.includes();
+      // expect(findSome('xyz')).toBe(true);
     });
   });
 
   describe('search for an empty string, is always true', function() {
     it('in an empty string', function() {
-      /* .... */
-      //expect(''.includes(x)).toBe(true);
+      const x = ['']
+      expect(''.includes(x)).toBe(true);
     });
     it('in `abc`', function() {
       /* .... */
