8a86dcd25eccb3987c3d95fb80fa68e8176f84ce
andrea morales <amoralesp88@gmail.com>
2021-01-18 20:52:53 -0600

    done

--- spec/koans.js
+++ spec/koans.js
@@ -557,8 +557,8 @@ describe('destructuring function parameters. ', () => {
         expect(id).toEqual(23);
         expect(name).toEqual('Bob');
       };
-      fn(23);
-
+      //fn(23);
+      fn(23, 'Bob');
     });
 
     it('for a missing array value', () => {
@@ -714,53 +714,64 @@ describe('spread with strings', () => {
 describe('class creation', () => {
 
   it('is as simple as `class XXX {}`', function() {
-   let TestClass = {};
+  class TestClass {};
    
     // const instance = new TestClass();
     //expect(typeof instance).toBe('object');
+    const instance = new TestClass();
+    expect(typeof instance).toBe('object');
   });
 
   it('class is block scoped', () => {
-    class Inside {}
+   // class Inside {}
     { class Inside {} }
     //expect(typeof Inside).toBe('undefined');
+    expect(typeof Inside).toBe('undefined');
+    
   });
 
   it('special method is `constructor`', function() {
     class User {
       constructor(id) {
-
+        this.id = id
       }
     }
 
     const user = new User(42);
     //expect(user.id).toEqual(42);
+    expect(user.id).toEqual(42);
   });
 
   it('defining a method is simple', function() {
     class User {
-
+      writesTests = () => false;
     }
 
     const notATester = new User();
     //expect(notATester.writesTests()).toBe(false);
+    expect(notATester.writesTests()).toBe(false);
   });
 
   it('multiple methods need no commas (opposed to object notation)', function() {
     class User {
-      wroteATest() { this.everWroteATest = true; }
-      isLazy()     {  }
+      constructor() {this.everWroteATest = false};
+      wroteATest() { this.everWroteATest = true; };
+      isLazy()     {return !this.everWroteATest  };
     }
 
     const tester = new User();
     //expect(tester.isLazy()).toBe(true);
+    expect(tester.isLazy()).toBe(true);
     tester.wroteATest();
     //expect(tester.isLazy()).toBe(false);
+    expect(tester.isLazy()).toBe(false);
   });
 
   it('anonymous class', () => {
-    const classType = typeof {};
+    //const classType = typeof {};
+    const classType = typeof (() => {});
     //expect(classType).toBe('function');
+    expect(classType).toBe('function');
   });
 
 });
